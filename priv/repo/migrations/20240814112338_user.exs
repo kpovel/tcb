0f5225e00c42b2ffedba6f539b8d40574712c366
Pavlo Karas <thepavlokaras@gmail.com>
2024-08-14 19:30:11 +0300

    feat: db schema

---
+++ priv/repo/migrations/20240814112338_user.exs
@@ -0,0 +1,40 @@
+defmodule Tcb.Repo.Migrations.User do
+  use Ecto.Migration
+
+  def change do
+    create table(:image) do
+      add :name, :string, null: false
+      add :value, :binary, null: false
+    end
+
+    create table(:validate_email_codes) do
+      add :code, :number, null: false
+      add :valited_email, :boolean, null: false, default: false
+    end
+
+    create table(:users) do
+      add :login, :string, null: false
+      add :nickname, :string
+      add :email, :string, null: false, unique_index: true
+      add :validated_email, :boolean, null: false, default: false
+      add :password, :string, null: false
+      add :onboarded, :boolean, null: false
+      add :avatar_id, references("image")
+      add :about_me, :string
+    end
+
+    create unique_index(:users, [:email])
+
+    create table(:refresh_tokens) do
+      add :user_id, references(:users), null: false
+      add :token, :string, null: false
+      add :expired_at, :date, null: false
+    end
+
+    create table(:access_tokens) do
+      add :user_id, references(:users), null: false
+      add :token, :string, null: false
+      add :expired_at, :utc_datetime, null: false
+    end
+  end
+end
