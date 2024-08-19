eed71c56ab36b26d789f658edea382283a2e5d67
Pavlo Karas <thepavlokaras@gmail.com>
2024-08-19 20:43:21 +0300

    feat: figured out how to set up gettext

--- lib/tcb_web/plugs/lang.ex
+++ lib/tcb_web/plugs/lang.ex
@@ -5,8 +5,12 @@ defmodule TcbWeb.Plugs.Lang do
   def init(default), do: default
 
   def call(%Plug.Conn{query_params: %{"lang" => lang}} = conn, _default) when lang in @langs do
+    Gettext.put_locale(lang)
     assign(conn, :lang, lang)
   end
 
-  def call(conn, default), do: assign(conn, :lang, default)
+  def call(conn, default) do
+    Gettext.put_locale(default)
+    assign(conn, :lang, default)
+  end
 end
