32417c1fce06b2ed0ab0b00a8a29b99d8581f6a7
Pavlo Karas <thepavlokaras@gmail.com>
2024-09-28 21:15:25 +0300

    feat: image add default_avatar column

--- lib/tcb/image.ex
+++ lib/tcb/image.ex
@@ -1,8 +1,17 @@
 defmodule Tcb.Image do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "image" do
     field :name, :string
     field :value, :binary
+    field :default_avatar, :boolean
+  end
+
+  def changeset(image, attrs) do
+    image
+    |> cast(attrs, [:name, :value, :default_avatar])
+    |> validate_required([:name, :value])
+    |> unique_constraint(:name)
   end
 end
