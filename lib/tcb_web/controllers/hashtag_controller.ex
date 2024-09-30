9e56c7a94c5dc067f80a519e4d1a886c14609e2d
Pavlo Karas <thepavlokaras@gmail.com>
2024-09-30 15:30:38 +0300

    feat: get all hashtags

--- lib/tcb_web/controllers/hashtag_controller.ex
+++ lib/tcb_web/controllers/hashtag_controller.ex
@@ -1,11 +1,18 @@
 defmodule TcbWeb.HashtagController do
   use TcbWeb, :controller
   alias Tcb.Repo
+  import Ecto.Query
 
   action_fallback TcbWeb.FallbackController
 
-  def index(conn, _params) do
-    hashtags = Tcb.Hashtag |> Repo.all()
+  def index(%Plug.Conn{assigns: %{lang: lang}} = conn, _params) do
+    hashtags =
+      from(Tcb.Hashtag,
+        where: [lang: ^lang],
+        select: [:category, :name, :hashtag_id]
+      )
+      |> Repo.all()
+
     render(conn, :index, hashtags: hashtags)
   end
 end
--- lib/tcb_web/controllers/hashtag_json.ex
+++ lib/tcb_web/controllers/hashtag_json.ex
@@ -1,12 +1,25 @@
 defmodule TcbWeb.HashtagJSON do
   def index(%{hashtags: hashtags}) do
-    for(hashtag <- hashtags, do: data(hashtag))
-  end
+    hashtags
+    |> Enum.reduce([], fn %Tcb.Hashtag{} = hashtag, acc ->
+      case List.first(acc) do
+        nil ->
+          [%{name: hashtag.category, hashtags: [%{id: hashtag.hashtag_id, name: hashtag.name}]}]
 
-  defp data(%Tcb.Hashtag{} = hashtag) do
-    %{
-      category: hashtag.category,
-      name: hashtag.name
-    }
+        %{name: name} when name == hashtag.category ->
+          List.update_at(acc, 0, fn res ->
+            Map.update!(res, :hashtags, fn hashtags ->
+              hashtags ++ [%{id: hashtag.hashtag_id, name: hashtag.name}]
+            end)
+          end)
+
+        _ ->
+          [
+            %{name: hashtag.category, hashtags: [%{id: hashtag.hashtag_id, name: hashtag.name}]}
+            | acc
+          ]
+      end
+    end)
+    |> Enum.reverse()
   end
 end
