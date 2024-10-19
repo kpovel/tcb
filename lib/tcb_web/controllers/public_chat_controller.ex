06fe22faaf5410b076db52a01fc683e401b076ca
Pavlo Karas <thepavlokaras@gmail.com>
2024-10-19 10:11:01 +0300

    feat: create a public chat with an image

--- lib/tcb_web/controllers/public_chat_controller.ex
+++ lib/tcb_web/controllers/public_chat_controller.ex
@@ -34,12 +34,55 @@ defmodule TcbWeb.PublicChatController do
 
   def create(
         %Plug.Conn{assigns: %{user: %Tcb.User{} = user}} = conn,
+        %{"chatRoom" => %Plug.Upload{} = chat_room, "picture" => %Plug.Upload{} = picture}
+      ) do
+    chat_name = chat_room_name(chat_room)
+    valid_image = valid_chat_image(picture)
+
+    case valid_image do
+      {:error, reason} ->
+        conn |> render(:create_error, %{chat_name: "", picture: reason})
+
+      {:ok} ->
+        {:ok, data} =
+          Repo.transaction(fn ->
+            %Tcb.Image{id: image_id} =
+              %Tcb.Image{
+                name: Ecto.UUID.generate() <> Path.extname(picture.filename),
+                value: File.read!(picture.path),
+                default_avatar: false
+              }
+              |> Repo.insert!()
+
+            %PublicChat{id: chat_id, uuid: chat_uuid} =
+              %PublicChat{}
+              |> PublicChat.changeset(%{
+                name: chat_name,
+                description: "",
+                uuid: Ecto.UUID.generate(),
+                created: false,
+                image_id: image_id
+              })
+              |> Repo.insert!()
+
+            %ChatMembers{}
+            |> ChatMembers.changeset(%{chat_id: chat_id, user_id: user.id, owner: true})
+            |> Repo.insert!()
+
+            conn |> render(:create, %{uuid: chat_uuid})
+          end)
+
+        data
+    end
+  end
+
+  def create(
+        %Plug.Conn{assigns: %{user: %Tcb.User{} = user}} = conn,
         %{"chatRoom" => chat_room}
       ) do
     chat_name = chat_room_name(chat_room)
 
-    %PublicChat{} =
-      chat =
+    %PublicChat{id: chat_id, uuid: chat_uuid} =
       %PublicChat{}
       |> PublicChat.changeset(%{
         name: chat_name,
@@ -47,45 +90,12 @@ defmodule TcbWeb.PublicChatController do
         uuid: Ecto.UUID.generate(),
         created: false
       })
-      |> IO.inspect()
       |> Repo.insert!()
 
     %ChatMembers{}
-    |> ChatMembers.changeset(%{chat_id: chat.id, user_id: user.id, owner: true})
+    |> ChatMembers.changeset(%{chat_id: chat_id, user_id: user.id, owner: true})
     |> Repo.insert!()
 
-    conn |> render(:create, %{uuid: chat.uuid})
-  end
-
-  def create(
-        %Plug.Conn{assigns: %{user: %Tcb.User{} = user}} = conn,
-        %{"chatRoom" => %Plug.Upload{} = chat_room, "picture" => %Plug.Upload{} = picture}
-      ) do
-    chat_name = chat_room_name(chat_room)
-
-    valid_upload = valid_chat_image(picture)
-
-    case valid_upload do
-      {:error, reason} -> conn |> send_resp(400, reason)
-      {:ok} -> nil
-    end
-
-    # Repo.transaction(fn ->
-    #   %Tcb.Image{id: image_id} =
-    #     %Tcb.Image{
-    #       name: Ecto.UUID.generate() <> Path.extname(image.filename),
-    #       value: File.read!(image.path),
-    #       default_avatar: false
-    #     }
-    #     |> Repo.insert!()
-    #
-    #   from(Tcb.User,
-    #     where: [id: ^user.id],
-    #     update: [set: [avatar_id: ^image_id]]
-    #   )
-    #   |> Repo.update_all([])
-    # end)
-
-    conn |> send_resp(400, "{}")
+    conn |> render(:create, %{uuid: chat_uuid})
   end
 end
--- lib/tcb_web/controllers/public_chat_json.ex
+++ lib/tcb_web/controllers/public_chat_json.ex
@@ -2,4 +2,8 @@ defmodule TcbWeb.PublicChatJSON do
   def create(%{uuid: uuid}) do
     %{uuid: uuid}
   end
+
+  def create_error(%{chat_name: chat_name, picture: picture}) do
+    %{chatName: chat_name, picture: picture}
+  end
 end
